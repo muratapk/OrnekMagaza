54f6ae96274b5deeeda1c6874fcab4c4e13b6238
muratapk <muratapk@hotmail.com>
2026-02-14 20:24:46 +0300

    Sepet işlemi window reload olayı

--- OrnekMagaza/Controllers/HomeController.cs
+++ OrnekMagaza/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace OrnekMagaza.Controllers
             //           where urun.ProductID == id
             //           select urun;
             //var urundetay = sorgu.FirstOrDefault();
-            var liste = _context.Products.Where(x=>x.ProductID==id).FirstOrDefault();
+            var liste = _context.Products.Where(x => x.ProductID == id).FirstOrDefault();
             if (liste == null)
             {
                 return NotFound("Ürün bulunamadı");
@@ -37,7 +37,7 @@ namespace OrnekMagaza.Controllers
                 return NotFound("Ürün bulunamadı");
             }
             List<Products> Cart;
-            var sessionCart= HttpContext.Session.GetString("Cart");
+            var sessionCart = HttpContext.Session.GetString("Cart");
             if (sessionCart != null)
             {
                 Cart = System.Text.Json.JsonSerializer.Deserialize<List<Products>>(sessionCart);
@@ -50,7 +50,7 @@ namespace OrnekMagaza.Controllers
             }
             Cart.Add(urun);
             HttpContext.Session.SetString("Cart", System.Text.Json.JsonSerializer.Serialize(Cart));
-           
+
             return Json("Ürün sepete eklendi");
         }
 
@@ -68,7 +68,29 @@ namespace OrnekMagaza.Controllers
             }
             return View();
         }
+        public IActionResult RemoveFromCart(int id)
+        {
+            var sessionCart = HttpContext.Session.GetString("Cart");
+            //sepettin tüm içeriğini al
+            if (sessionCart != null)
+            {
+                //içerik boş degilse 
+                var Cart = System.Text.Json.JsonSerializer.Deserialize<List<Products>>(sessionCart);
+                //session get ile verileri json formatında al ve liste tipine çevir
+                var urunToRemove = Cart.FirstOrDefault(x => x.ProductID == id);
+                //bu listeden id'si eşit olan ürünü bul
+                if (urunToRemove != null)
+                {
+                    //ürün bulunursa sepetten çıkar
+                    Cart.Remove(urunToRemove);
+
+                    HttpContext.Session.SetString("Cart", System.Text.Json.JsonSerializer.Serialize(Cart));
+                    //session 'a güncellenmiş sepeti json formatında kaydet
+                    return RedirectToAction("AddToList","Home");
+                }
+            }
+            return NotFound("Ürün bulunamadı");
+        }
 
     }
-    
 }
