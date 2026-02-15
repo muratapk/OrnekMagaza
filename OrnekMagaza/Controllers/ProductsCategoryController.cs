a108be62245668e706ef0fecdd595bee352f738b
muratapk <muratapk@hotmail.com>
2026-02-15 21:11:57 +0300

    ürün kategorisi ekleme

---
+++ OrnekMagaza/Controllers/ProductsCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrnekMagaza.Data;
+
+namespace OrnekMagaza.Controllers
+{
+    public class ProductsCategoryController : Controller
+    {
+        private readonly MagazaDb _context;
+        public ProductsCategoryController(MagazaDb context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var productsCategories = _context.Products.Include(x => x.Category).ToList();
+            return View(productsCategories);
+        }
+    }
+}
